2724afd0c9ef0a25176924c99a2260e4809a7028
Danilo Téo <danilo.teo@liven.tech>
2020-12-08 09:07:03 -0300

    feat: begin styling of left menubar

---
+++ src/components/MenuBar/styles.tsx
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components';
+import { 
+    Home,
+    Notifications,
+    Email,
+    FavoriteBorder,
+    Person,
+    ExitToApp,
+ } from '../../styles/Icons'
+
+export const Container = styled.div`
+    display: none;
+    @media(min-width:500px){
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        position: sticky;
+        top: 0;
+        left: 0;
+        padding: 9px 19px 20px;
+        max-height: 100vh;
+        overflow-y: auto;
+    }
+
+
+
+`;
+
+export const Topside = styled.div``;
+
+export const MenuButton = styled.div``;
+
+export const Logo = styled.div``;
+
+const iconCSS = css`
+    flex-shrink: 0;
+    width: 30px;
+    height: 30px;
+    color: var(--white);
+`
+
+export const HomeIcon = styled(Home)`${iconCSS}`;
+
+export const BellIcon = styled(Notifications)`${iconCSS}`;
+
+export const EmailIcon = styled(Email)`${iconCSS}`;
+
+export const FavoriteIcon = styled(FavoriteBorder)`${iconCSS}`;
+
+export const ProfileIcon = styled(Person)`${iconCSS}`;
