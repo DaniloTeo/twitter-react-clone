5d875cd815433c32cf888907e3a75bfcacb27636
Danilo Téo <danilo.teo@liven.tech>
2021-02-02 16:36:46 -0300

    34343

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -37,7 +37,7 @@ const MenuBar: React.FC = () => {
                 </MenuButton>
                 <MenuButton>
                     <FavoriteIcon/>
-                    <span>Favoritados</span>
+                    <span>Favorfdsafdsafdsitados</span>
                 </MenuButton>
                 <MenuButton className="active">
                     <ProfileIcon/>
