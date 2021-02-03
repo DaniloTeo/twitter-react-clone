961465963b775a296b88b7146cfdc67ac7cd3edf
Danilo Téo <danilo.teo@liven.tech>
2021-02-03 08:32:54 -0300

    dsadsa

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import Button from '../Button'
+
+import { 
+    Container,
+    Topside,
+    MenuButton,
+    HomeIcon,
+    BellIcon,
+    EmailIcon,
+    FavoriteIcon,
+    ProfileIcon,
+    Logo,
+    BotSide,
+    Avatar,
+    ProfileData,
+    ExitIcon
+} from './styles';
+
+const MenuBar: React.FC = () => {
+  return (
+      <Container>
+          <Topside>
+              <Logo/>
+              <MenuButton>
+                    <HomeIcon/>
+                    <span>Página fdsafdasfds</span>
+                </MenuButton>    
+                <MenuButton>
+                    <BellIcon/>
+                    <span>Notifdsafdsadsficações</span>
+                </MenuButton>
+                <MenuButton>
+                    <EmailIcon/>
+                    <span>Mensafdsfdsafdsgens</span>
+                </MenuButton>
+                <MenuButton>
+                    <FavoriteIcon/>
+                    <span>Favorfdsafdsafdsitados</span>
+                </MenuButton>
+                <MenuButton className="active">
+                    <ProfileIcon/>
+                    <span>Perfil</span>
+                </MenuButton>
+                <Button>
+                    <span>Tweetar</span>
+                </Button>
+          </Topside>
+
+          <BotSide>
+              <Avatar/>
+              <ProfileData>
+                  <strong>Danilo Téo</strong>
+                  <span>@teo_danilo</span>
+              </ProfileData>
+              <ExitIcon/>
+          </BotSide>
+      </Container>
+  );
+}
+
+export default MenuBar;
